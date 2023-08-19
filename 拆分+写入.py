d20c3954c9299e4250657ed3a0799e400d085ace
LiebknechtLi <140191631+LiebknechtLi@users.noreply.github.com>
2023-08-19 19:57:54 +0800

    Update 拆分+写入.py

--- 拆分+写入.py
+++ 拆分+写入.py
@@ -1,12 +1,14 @@
 import pandas as pd
-df=pd.read_excel('C:/Users/Administrator/Desktop/Copy of 2023年5月数据-CT.xls','PWC5月开票') #读取的文件地址及表单号
-print(df.columns) #检查列名
-split_list=df['Billing号码'].str.split(',') #拆分”Billing号码“列
-flat_list = split_list.explode().astype(int, errors='ignore') #将拆分出的多列摊开为一行
-flat_list = flat_list.reset_index(drop=True) #将上述行还原为一列
+df=pd.read_excel('C:/Users/Administrator/Desktop/Copy of 2023年5月数据-CT.xls','PWC5月开票') #The address of the file to be read and the sheet
+print(df.columns) #Check the index of columns
+
+split_list=df['Billing号码'].str.split(',') #Split "Billing号码" column
+flat_list = split_list.explode().astype(int, errors='ignore') #Spreading split multiple columns into a single row
+flat_list = flat_list.reset_index(drop=True) #Reduce the above rows to one column
 print(flat_list)
-writer = pd.ExcelWriter('C:/Users/Administrator/Desktop/output.xls') #写入文件的地址
-flat_list.to_excel(writer, sheet_name='Sheet1') #写入上一行的指定文件
+
+writer = pd.ExcelWriter('C:/Users/Administrator/Desktop/output.xls') #The address of the file to be written
+flat_list.to_excel(writer, sheet_name='Sheet1') #Write flat_list in the appointed file
 writer.save()
 
 
